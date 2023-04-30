0bcdb20189754fa6d670daf3939c1806177a7a9c
hir0o <mk.9.q.kyu@gmail.com>
2023-04-30 18:20:59 +0900

    もっとリファクタ

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 use clap::Parser;
 use image::io::Reader as ImageReader;
+use std::path::{Path, PathBuf};
 use thiserror::Error;
 
 #[derive(Debug, Error)]
@@ -8,7 +9,7 @@ enum CliError {
     #[error("ディレクトリではありません")]
     NotADirectory,
     #[error("ファイルの変換に失敗しました: [{0}]")]
-    ConvertError(String),
+    ConvertError(#[from] image::ImageError),
 }
 
 #[derive(Parser, Debug)]
@@ -26,37 +27,39 @@ fn main() {
     }
 }
 
-fn convert_jpg_to_png(path: &std::path::PathBuf) -> Result<(), CliError> {
+fn convert_jpg_to_png(path: &PathBuf) -> Result<(), CliError> {
     let img = ImageReader::open(path)
-        .map_err(|_| CliError::NotADirectory)
-        .and_then(|reader| {
-            reader
-                .decode()
-                .map_err(|err| CliError::ConvertError(err.to_string()))
-        })?;
+        .map_err(|_| CliError::NotADirectory)?
+        .decode()
+        .map_err(CliError::ConvertError)?;
     let mut new_path = path.to_path_buf();
     println!("{} -> {}", path.display(), new_path.display());
     new_path.set_extension("png");
-    img.save(new_path)
-        .map_err(|err| CliError::ConvertError(err.to_string()))?;
+    img.save(new_path).map_err(CliError::ConvertError)?;
 
     Ok(())
 }
 
-fn convert_manager(path: &std::path::Path) -> Result<(), CliError> {
+fn convert_manager(path: &Path) -> Result<(), CliError> {
     if !path.is_dir() {
         return Err(CliError::NotADirectory);
     }
 
-    let dir_item = path.read_dir().map_err(|_| CliError::NotADirectory)?;
+    let dir_entries = path.read_dir().map_err(|_| CliError::NotADirectory)?;
 
-    for item in dir_item {
-        let path = item.map_err(|_| CliError::NotADirectory)?.path();
+    for entry in dir_entries {
+        let path = entry.map_err(|_| CliError::NotADirectory)?.path();
         if path.is_dir() {
             convert_manager(&path)?;
         } else if let Some(extension) = path.extension() {
             if extension == "jpg" {
-                convert_jpg_to_png(&path)?;
+                if let Err(e) = convert_jpg_to_png(&path) {
+                    println!(
+                        "failed convert {}, error: {}",
+                        path.to_str().unwrap_or("?"),
+                        e
+                    );
+                }
             }
         }
     }
